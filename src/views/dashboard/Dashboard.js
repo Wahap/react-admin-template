7cd0701e0ca8e95fc2dd1387b11cc68708f8fce7
Dmr Solutions <demir.vahap@hotmail.com>
2023-03-20 21:27:15 +0000

    account added

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -3,12 +3,14 @@ import axios from 'axios'
 import { CButton, CCol, CForm, CFormInput, CFormFeedback, CFormLabel } from '@coreui/react'
 
 const baseURL = 'https://cps-azure-func.azurewebsites.net/api/addAddress'
+const baseURLAccount = 'https://cps-azure-func.azurewebsites.net/api/addAccount'
 
 const Dashboard = () => {
   const [validated, setValidated] = useState(false)
   const [address1, setAddress1] = useState('default')
   const [address2, setAddress2] = useState('default')
   const [postcode, setPostCode] = useState('default')
+  const [name, setAccountName] = useState('default')
 
   const handleSubmit = (event) => {
     const form = event.currentTarget
@@ -21,50 +23,95 @@ const Dashboard = () => {
     const address = { address1: address1, address2: address2, postcode: postcode }
     axios.post(baseURL, address)
   }
+
+  const handleAccountSubmit = (event) => {
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    setValidated(true)
+    const account = { name: name }
+    axios.post(baseURLAccount, account)
+  }
+
   return (
-    <CForm className="row g-3 needs-validation" validated={validated} onSubmit={handleSubmit}>
-      <CCol md={4}>
-        <CFormLabel htmlFor="address1">Address1</CFormLabel>
-        <CFormInput
-          type="text"
-          id="address1"
-          required
-          onChange={(e) => {
-            setAddress1(e.target.value)
-          }}
-        />
-        <CFormFeedback invalid>Please provide a valid Address1.</CFormFeedback>
-      </CCol>
-      <CCol md={4}>
-        <CFormLabel htmlFor="address2">Address2</CFormLabel>
-        <CFormInput
-          type="text"
-          id="address2"
-          required
-          onChange={(e) => {
-            setAddress2(e.target.value)
-          }}
-        />
-        <CFormFeedback invalid>Please provide a valid Address2.</CFormFeedback>
-      </CCol>
-      <CCol md={4}>
-        <CFormLabel htmlFor="postcode">Post Code</CFormLabel>
-        <CFormInput
-          type="text"
-          id="postcode"
-          required
-          onChange={(e) => {
-            setPostCode(e.target.value)
-          }}
-        />
-        <CFormFeedback invalid>Please provide a valid Post Code.</CFormFeedback>
-      </CCol>
-      <CCol xs={12}>
-        <CButton color="primary" type="submit">
-          Submit form
-        </CButton>
-      </CCol>
-    </CForm>
+    <>
+      <div>
+        <div>
+          {' '}
+          <CForm className="row g-3 needs-validation" validated={validated} onSubmit={handleSubmit}>
+            <CCol md={4}>
+              <CFormLabel htmlFor="address1">Address1</CFormLabel>
+              <CFormInput
+                type="text"
+                id="address1"
+                required
+                onChange={(e) => {
+                  setAddress1(e.target.value)
+                }}
+              />
+              <CFormFeedback invalid>Please provide a valid Address1.</CFormFeedback>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="address2">Address2</CFormLabel>
+              <CFormInput
+                type="text"
+                id="address2"
+                required
+                onChange={(e) => {
+                  setAddress2(e.target.value)
+                }}
+              />
+              <CFormFeedback invalid>Please provide a valid Address2.</CFormFeedback>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="postcode">Post Code</CFormLabel>
+              <CFormInput
+                type="text"
+                id="postcode"
+                required
+                onChange={(e) => {
+                  setPostCode(e.target.value)
+                }}
+              />
+              <CFormFeedback invalid>Please provide a valid Post Code.</CFormFeedback>
+            </CCol>
+            <CCol xs={12}>
+              <CButton color="primary" type="submit">
+                Submit form
+              </CButton>
+            </CCol>
+          </CForm>
+        </div>
+        <div>
+          <CForm
+            className="row g-3 needs-validation"
+            validated={validated}
+            onSubmit={handleAccountSubmit}
+          >
+            <CCol md={4}>
+              <CFormLabel htmlFor="name">Account Name</CFormLabel>
+              <CFormInput
+                type="text"
+                id="name"
+                required
+                onChange={(e) => {
+                  setAccountName(e.target.value)
+                }}
+              />
+              <CFormFeedback invalid>Please provide a valid Name.</CFormFeedback>
+            </CCol>
+
+            <CCol xs={12}>
+              <CButton color="primary" type="submit">
+                Submit form
+              </CButton>
+            </CCol>
+          </CForm>
+        </div>
+      </div>
+    </>
   )
 }
 
